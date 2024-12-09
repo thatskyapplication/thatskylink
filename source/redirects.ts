844d448f1a4ef033051f60283a5ad0f366e9075a
Jiralite <33201955+Jiralite@users.noreply.github.com>
2024-12-09 03:35:57 +0000

    feat: More redirects

--- source/redirects.ts
+++ source/redirects.ts
@@ -1,1 +1,22 @@
-export const REDIRECTS = new Map([["skycord", "https://discord.gg/thatskygame"]]);
+export const REDIRECTS = new Map<string, `https://${string}`>([
+	["skycord", "https://discord.gg/thatskygame"],
+	["skydc", "https://discord.gg/thatskygame"],
+	["skydiscord", "https://discord.gg/thatskygame"],
+	["skyfacebook", "https://facebook.com/thatskygame"],
+	["skyfb", "https://facebook.com/thatskygame"],
+	["skyig", "https://instagram.com/thatskygame"],
+	["skyinsta", "https://instagram.com/thatskygame"],
+	["skyinstagram", "https://instagram.com/thatskygame"],
+	["skymerch", "https://thatskyshop.com"],
+	["skymerchandise", "https://thatskyshop.com"],
+	["skyreddit", "https://reddit.com/r/SkyGame"],
+	["skyreddit2", "https://reddit.com/r/SkyChildrenOfLight"],
+	["skytwitter", "https://x.com/thatskygame"],
+	["skyx", "https://x.com/thatskygame"],
+	["skyyoutube", "https://youtube.com/@thatgamecompany"],
+	["skyyt", "https://youtube.com/@thatgamecompany"],
+	["skywebstore", "https://webshop.thatskygame.com"],
+	["tgc", "https://thatgamecompany.com"],
+	["tsa", "https://thatskyapplication.com"],
+	["tsg", "https://thatskygame.com"],
+]);
