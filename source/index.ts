caaf26263fa97178bfecc9dc1f5d627e0f3d75aa
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-11-23 20:31:50 +0000

    feat: Version worker

--- source/index.ts
+++ source/index.ts
@@ -3,32 +3,40 @@ import { REDIRECTS } from "./redirects.js";
 
 interface Env {
 	SENTRY_DATA_SOURCE_NAME: string;
+	CF_VERSION_METADATA: WorkerVersionMetadata;
 }
 
-export default withSentry((env) => ({ dsn: env.SENTRY_DATA_SOURCE_NAME, sendDefaultPii: true }), {
-	// Need env here so Sentry does not see it as unknown.
-	fetch(request, _env) {
-		if (!(request.method === "GET" || request.method === "HEAD")) {
-			return new Response(null, { status: 405 });
-		}
-
-		const url = new URL(request.url);
-		const pathname = url.pathname;
-
-		if (pathname.startsWith("/profiles/")) {
-			const userId = pathname.slice(10);
-
-			if (userId) {
-				return Response.redirect(`https://thatskyapplication.com/sky-profiles/${userId}`, 301);
+export default withSentry(
+	(env) => ({
+		dsn: env.SENTRY_DATA_SOURCE_NAME,
+		release: env.CF_VERSION_METADATA.id,
+		sendDefaultPii: true,
+	}),
+	{
+		// Need env here so Sentry does not see it as unknown.
+		fetch(request, _env) {
+			if (!(request.method === "GET" || request.method === "HEAD")) {
+				return new Response(null, { status: 405 });
 			}
-		}
 
-		const redirect = REDIRECTS.get(url.pathname.slice(1).toLowerCase());
+			const url = new URL(request.url);
+			const pathname = url.pathname;
 
-		if (redirect) {
-			return Response.redirect(redirect, 301);
-		}
+			if (pathname.startsWith("/profiles/")) {
+				const userId = pathname.slice(10);
 
-		return Response.redirect("https://github.com/thatskyapplication/thatskylink", 301);
-	},
-} satisfies ExportedHandler<Env>);
+				if (userId) {
+					return Response.redirect(`https://thatskyapplication.com/sky-profiles/${userId}`, 301);
+				}
+			}
+
+			const redirect = REDIRECTS.get(url.pathname.slice(1).toLowerCase());
+
+			if (redirect) {
+				return Response.redirect(redirect, 301);
+			}
+
+			return Response.redirect("https://github.com/thatskyapplication/thatskylink", 301);
+		},
+	} satisfies ExportedHandler<Env>,
+);
